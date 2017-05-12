03d35568fd3123756898664d32787c163b49f830
Michael Shennan <m.shennan22@gmail.com>
2017-05-12 14:59:44 +0100

    hero can talk and eating food increases health

--- hero.js
+++ hero.js
@@ -3,12 +3,17 @@ function Hero(name, health, favouriteFood) {
   this.health = health;
   this.favouriteFood = favouriteFood;
   this.tasks = [];
+  this.gold = 0;
 }
 
 Hero.prototype = {
 
   talk: function() {
     return "My name is " + this.name;
+  },
+
+  eatFood: function(food) {
+    return this.health += food.replenishmentValue;
   }
 
 }
--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -1,12 +1,15 @@
 var assert = require("assert");
 var Hero = require("../hero.js");
+var Food = require("../food.js");
 
 describe("Hero", function() {
 
   var hero1;
+  var food1;
 
   beforeEach(function() {
     hero1 = new Hero("Gardven", 100, "chicken");
+    food1 = new Food("chicken", 20);
   });
 
   it("should have a name", function() {
@@ -18,7 +21,16 @@ describe("Hero", function() {
   });
 
   it("should have a favourite food", function() {
-    assert.equal("chicken", hero1.favouriteFood)
+    assert.equal("chicken", hero1.favouriteFood);
+  });
+
+  it("can talk", function() {
+    assert.equal("My name is Gardven", hero1.talk());
+  })
+
+  it("eating food should increase hero health", function() {
+    hero1.eatFood(food1);
+    assert.equal(120, hero1.health);
   });
 
 });
