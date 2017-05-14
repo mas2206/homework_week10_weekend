8be86c1315a7b91ff76c766552824c7c790c0d2d
Michael Shennan <m.shennan22@gmail.com>
2017-05-14 17:50:35 +0100

    rat can poison food test passes

---
+++ specs/rat_spec.js
@@ -0,0 +1,20 @@
+var assert = require("assert");
+var Rat = require("../rat.js");
+var Food = require("../food.js");
+
+describe("Rat", function() {
+
+  var rat1;
+  var food1;
+
+  beforeEach(function() {
+    rat1 = new Rat();
+    food1 = new Food("chicken", 20, false);
+  });
+
+  it("can poison food", function() {
+    rat1.poisonFood(food1);
+    assert.equal(true, food1.poisoned);
+  });
+
+});
